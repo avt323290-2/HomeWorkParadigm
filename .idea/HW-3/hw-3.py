e2f897207a7751472931ddcb675888675c4513d2
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-11-09 09:44:29 +0300

    Добавили решение третьего дз

--- .idea/HW-3/hw-3.py
+++ .idea/HW-3/hw-3.py
@@ -16,3 +16,46 @@
 терминал с вашего компьютера.
 """
 
+# Для решения задачи воспользуемся процедурным стилем программирования на Python.
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for i in range(3):
+        if all([board[i][j] == player for j in range(3)]) or all([board[j][i] == player for j in range(3)]):
+            return True
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
+
+def play_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+    moves = 0
+
+    while True:
+        print_board(board)
+        print(f"Ход игрока {current_player}")
+        row, col = map(int, input("Введите номер строки и столбца (например, '1 2'): ").split())
+
+        if board[row - 1][col - 1] == " ":
+            board[row - 1][col - 1] = current_player
+            moves += 1
+
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Игрок {current_player} победил!")
+                break
+            elif moves == 9:
+                print_board(board)
+                print("Ничья!")
+                break
+            else:
+                current_player = "O" if current_player == "X" else "X"
+        else:
+            print("Эта клетка уже занята. Попробуйте снова.")
+
+if __name__ == "__main__":
+    play_game()
