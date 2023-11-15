8894e6e73b9774fd153bbeb07fbdb9211a73c665
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-11-15 08:54:37 +0300

    Добавили автодокументацию в решение шестого дз

--- .idea/HW-6/hw-6.py
+++ .idea/HW-6/hw-6.py
@@ -15,19 +15,29 @@
 элемента нет в массиве.
 """
 def binary_search(arr, target):
+    """
+    Выполняет бинарный поиск элемента в отсортированном массиве.
+
+    :param arr: Отсортированный массив, в котором ищется элемент.
+    :type arr: list[int]
+    :param target: Искомый элемент.
+    :type target: int
+    :return: Индекс элемента в массиве или -1, если элемент не найден.
+    :rtype: int
+    """
     low, high = 0, len(arr) - 1
 
     while low <= high:
         mid = (low + high) // 2
 
         if arr[mid] == target:
-            return mid
+            return mid  # Нашли элемент, возвращаем индекс
         elif arr[mid] < target:
-            low = mid + 1
+            low = mid + 1  # Искомый элемент находится в правой половине
         else:
-            high = mid - 1
+            high = mid - 1  # Искомый элемент находится в левой половине
 
-    return -1
+    return -1  # Элемент не найден
 
 # Пример использования:
 sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
